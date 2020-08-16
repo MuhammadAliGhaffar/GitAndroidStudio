03bb635b40dc9a761f8397debac438b313e35406
MuhammadAliGhaffar <alibrohi999@gmail.com>
2020-08-16 13:35:38 +0500

    Adding Log Msg in MainActivity.java 2

--- app/src/main/java/com/example/gitandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/gitandroidstudio/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("Tag","For Testing Code");
+        Log.i("Tag","For Testing Code");
     }
 }
